20268d01f17db4bd61a6275f4d8d1e05838e98c7
Anthony <anthony@Anthonys-MacBook-Air.local>
2026-04-05 11:15:52 -0700

    Able to get acc and gyro values

--- ar-probe/src/main.rs
+++ ar-probe/src/main.rs
@@ -2,33 +2,41 @@
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::i2c::{I2cConfig, I2cDriver};
+use esp_idf_hal::units::Hertz;
+
+
+
+use mpu6050::Mpu6050;
+
+
+
 
 fn main() {
     esp_idf_svc::sys::link_patches();
-
-
-    let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::output(peripherals.pins.gpio48).unwrap();
-    let mut gled = PinDriver::output(peripherals.pins.gpio46).unwrap();
-    
-
-
-    // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    let peripherals = Peripherals::take().unwrap();
+
+    let mpu_sda = peripherals.pins.gpio11;
+    let mpu_scl = peripherals.pins.gpio12;
+
+    let config = I2cConfig::new().baudrate(Hertz(400_000));
+    let i2c = I2cDriver::new(peripherals.i2c0, mpu_sda, mpu_scl, &config).unwrap();
+    let mut mpu = Mpu6050::new(i2c);
+    
+    mpu.init(&mut esp_idf_hal::delay::FreeRtos).unwrap();
 
 
-    led.set_low().unwrap();
-    gled.set_low().unwrap();
+
 
     loop {
-        gled.set_low().unwrap();
-        led.set_high().unwrap();
-        FreeRtos::delay_ms(500);
-        gled.set_high().unwrap();
-        led.set_low().unwrap();
-        FreeRtos::delay_ms(500);
+        let accel = mpu.get_acc().unwrap();
+        let gyro = mpu.get_gyro().unwrap();
+        log::info!("Accel: x={:.2} y={:.2} z={:.2}", accel.x, accel.y, accel.z);
+        log::info!("Gyro:  x={:.2} y={:.2} z={:.2}", gyro.x, gyro.y, gyro.z);
+        FreeRtos::delay_ms(100);
+
 
     }
 }
