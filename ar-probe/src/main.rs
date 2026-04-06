f3e357bbaad3cd898e4005302169807360230cee
Anthony <anthony@Anthonys-MacBook-Air.local>
2026-04-05 18:57:07 -0700

    adding temp data

--- ar-probe/src/main.rs
+++ ar-probe/src/main.rs
@@ -1,6 +1,5 @@
 
 use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::i2c::{I2cConfig, I2cDriver};
 use esp_idf_hal::units::Hertz;
@@ -8,8 +7,8 @@ use esp_idf_hal::units::Hertz;
 
 
 use mpu6050::Mpu6050;
-
-
+use bmp280_ehal::{BMP280, Control, Oversampling, PowerMode};
+use shared_bus::BusManagerSimple;
 
 
 fn main() {
@@ -18,23 +17,45 @@ fn main() {
 
     let peripherals = Peripherals::take().unwrap();
 
-    let mpu_sda = peripherals.pins.gpio11;
-    let mpu_scl = peripherals.pins.gpio12;
+    let sda = peripherals.pins.gpio11;
+    let scl = peripherals.pins.gpio12;
+
+
+
 
     let config = I2cConfig::new().baudrate(Hertz(400_000));
-    let i2c = I2cDriver::new(peripherals.i2c0, mpu_sda, mpu_scl, &config).unwrap();
-    let mut mpu = Mpu6050::new(i2c);
+    let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config).unwrap();
     
-    mpu.init(&mut esp_idf_hal::delay::FreeRtos).unwrap();
+    let bus = BusManagerSimple::new(i2c);
+
+    let mut mpu = Mpu6050::new(bus.acquire_i2c());
+    mpu.init(&mut FreeRtos).unwrap();
 
 
+    let mut bmp = BMP280::new(bus.acquire_i2c()).unwrap();
+    bmp.reset();
+
+    bmp.set_control(Control {
+        osrs_t: Oversampling::x8,   // temperature oversampling
+        osrs_p: Oversampling::x8,   // pressure oversampling
+        mode: PowerMode::Normal,
+    });
 
 
     loop {
         let accel = mpu.get_acc().unwrap();
         let gyro = mpu.get_gyro().unwrap();
         log::info!("Accel: x={:.2} y={:.2} z={:.2}", accel.x, accel.y, accel.z);
-        log::info!("Gyro:  x={:.2} y={:.2} z={:.2}", gyro.x, gyro.y, gyro.z);
+
+
+        let pressure = bmp.pressure() / 100.0;
+        let bmp_temp = bmp.temp();
+        let bmp_tempF = (bmp_temp.clone() * 1.8) + 32.0; 
+        log::info!("BMP Temp: {:.2} C\n{:.2} F,\nPressure: {:.2} hPa", bmp_temp, bmp_tempF, pressure);
+
+
+
+
         FreeRtos::delay_ms(100);
 
 
